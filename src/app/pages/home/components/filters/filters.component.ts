8ea5ab28465c9fceedf217cdfd8b38737ad2a5c8
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-10-07 13:25:39 +0200

    app product box component

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+})
+export class FiltersComponent {
+  @Output() showCategory = new EventEmitter<string>();
+
+  categories = ['shoes', 'sports'];
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [],
 })
 export class HomeComponent implements OnInit {
+  cols = 3;
+  category: string | undefined;
+  rowHeight = ROWS_HEIGHT[this.cols];
+
   ngOnInit(): void {}
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+    console.log(this.category);
+  }
 }
